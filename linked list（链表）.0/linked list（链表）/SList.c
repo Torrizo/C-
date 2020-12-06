742753c01c318602cd3c9368950857abf8e384f2
Torrizo <1098902428@qq.com>
2020-12-06 23:34:37 +0800

    链表主函数

---
+++ linked list（链表）.0/linked list（链表）/SList.c
@@ -0,0 +1,30 @@
+#include "sList.h"
+
+void TestSList1()
+{
+	SListNode* pList = NULL;
+	SListPushBack(&pList, 1);
+	SListPushBack(&pList, 2);
+	SListPushBack(&pList, 3);
+	SListPushBack(&pList, 4);
+
+	SListPrint(pList);
+
+	SListPushFront(&pList, 0);
+	SListPrint(pList);
+
+	SListPopBack(&pList);
+	SListPopBack(&pList);
+	SListPopBack(&pList);
+	SListPopBack(&pList);
+	SListPopBack(&pList);
+	SListPopBack(&pList);
+	SListPrint(pList);
+}
+
+int main()
+{
+	TestSList1();
+
+	return 0;
+}
