8450eb337fd1c605c2149cd3cd216db67891c8d7
Torrizo <55399393+Torrizo@users.noreply.github.com>
2021-01-11 23:39:01 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,107 +1,41 @@
-//#include<iostream>
-//#include<vector>
-//#include<stack>
-//using namespace std;
-////struct TreeNode
-////{
-////	int val;
-////	TreeNode *left;
-////	TreeNode *right;
-////	TreeNode(int x) :val(x), left(nullptr), right(nullptr){}
-////	TreeNode(int x,TreeNode *left,TreeNode *right) : val(x), left(nullptr), right(nullptr){}
-////};
-////前序遍历
-//class  Solution
-//{
-//public:
-//	vector<int>preorderTraversal(TreeNode* root){
-//		stack<TreeNode*> st; 
-//		TreeNode* cur = root;
-//		vector<int> vec; 
-//		while (cur || st.empty())
-//		{
-//			//首先访问最左路径节点
-//			while (cur)
-//			{
-//				//访问根
-//				vec.push_back(cur->val);
-//				st.push(cur);
-//				cur = cur->left;
-//			}
-//			cur = st.top();
-//			st.pop();
-//			//访问右子树
-//			cur = cur->right;
-//		}
-//		return vec;
-//	}
-//};
-////中序遍历
-//class  Solution
-//{
-//public:
-//	vector<int>preorderTraversal(TreeNode* root){
-//		stack<TreeNode*> st;
-//		TreeNode* cur = root;
-//		vector<int> vec;
-//		while (cur || st.empty())
-//		{
-//			//首先访问最左路径节点
-//			while (cur)
-//			{
-//				//左子树
-//				st.push(cur);
-//				cur = cur->left;
-//			}
-//			//节点
-//			TreeNode * top = st.top();
-//			st.pop();
-//			vec.push_back(top->val);
-//			//访问右子树
-//			cur = cur->right;
-//		}
-//		return vec;
-//	}
-//}; 
-//struct TreeNode
-//{
-//	int val;
-//	TreeNode *left;
-//	TreeNode *right;
-//	TreeNode() :val(0), left(nullptr), right(nullptr){}
-//	TreeNode(int x) : val(x), left(nullptr), right(nullptr){}
-// 	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
-//};
-//class Soluion
-//{
-//public:
-//	vector<int> postorderTraversal(TreeNode* root){
-//		stack<TreeNode*> st;
-//		vector<int> vec;
-//		TreeNode* cur = root;
-//		while (cur || !st.empty())
-//		{
-//			while (cur)
-//			{
-//				st.push(cur);
-//				cur = cur->left;
-//			}
-//			TreeNode top = st.top();
-//			if (top->right == nullptr ||  top->right == prev)
-//			{
-//				vec.push_back(top->val);
-//				st.pop();
-//				prev = top;
-//			}
-//			else
-//			{
-//				//如果右子树没有访问，首先访问右子树
-//				cur = top->right;
-//			}
-//		}
-//	}
-//};
-//int mian()
-//{
-//	return 0;
-//}
+#include<iostream>
+//#include<list>
+using namespace std;
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+		val(x), next(NULL) {
+	}
+};
+class Solution {
+public:
+	ListNode* ReverseList(ListNode* pHead) {
+		if (pHead == nullptr)
+		{
+			return nullptr;
+		}
+		ListNode* p_fast = pHead->next;
+		pHead->next = NULL;
+		while (p_fast != NULL)
+		{
+			ListNode* temp = p_fast->next;
+			p_fast->next = pHead;
+			pHead = p_fast;
+			p_fast = temp;
+		}
+		return pHead;
+
+	}
+};
+void test()
+{
+	
+}
+int main()
+{
+	
+	test();
+	system("pasue");
+	return 0;
+}
