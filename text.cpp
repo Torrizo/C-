b281dda797b594721e48e5e2966279fa7b73a95c
Torrizo <55399393+Torrizo@users.noreply.github.com>
2021-03-07 00:19:45 +0800

    Add files via upload

--- text.cpp
+++ text.cpp
@@ -1,70 +1,194 @@
 #include<iostream>
 using namespace std;
-//class A {
-//public:
-//	A(int a)
-//		:_a1(a)
-//		, _a2(_a1)
-//	{}
-//
-//	void Print() {
-//		cout << _a1 << " " << _a2 << endl;
-//	}
-//private:
-//	int _a2;
-//	int _a1;
-//};
-//class A {
-//public:
-//	A() { ++_scount; }
-//	A(const A& t) { ++_scount; }
-//	static int GetACount() { return _scount; }
-//private:
-//	static int _scount;
-//};
-//int A::_scount = 0;
-//void TestA()
+
+//class Solution
 //{
-//	cout << A::GetACount() << endl;
-//	A a1, a2;
-//	A a3(a1);
-//	cout << A::GetACount() << endl;
+//public:
+//	Solution Print()
+//	{
+//		cout << " Solution()" << endl;
+//	}
+//	int Sum_Solutin(int n)
+//	{
+//		cout << "~Solution()" << endl;
+//		return n;
+//	}
+//};
+//int main()
+//{
+//	Solution s1;
+//	s1.Sum_Solutin(10); //S1生命周期在这一行
+//
+//
+//	//Solution();//匿名对象
+//	Solution().Sum_Solutin(10);   //只有这一行会使用这个创建对象，别人不需要使用
+//                                   //生命周期在这一行
+//
+//
+//	return 0;
 //}
-class B {
+
+//int main()
+//{
+//	static int monthDays[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+//	int year; int month; int day;
+//	// 这样可以不断接收输入  按ctrl+c结束
+//	while (cin >> year >> month >> day)
+//	{
+//		int n = 0;
+//		for (int i = 1; i < month; ++i)
+//		{
+//			n += monthDays[i];
+//		}
+//		n += day;
+//		if (month > 0 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
+//		{
+//			n += 1;
+//		}
+//		cout << n << endl;
+//	}
+//	return 0;
+//}
+
+
+
+class A
+{
 public:
-	B(int b = 0)
-		:_b(b)
-	{}
-	int _b;
-};
-class A {
-public:
-	void Print()
+	A()
 	{
-		cout << a << endl;
-		cout << b._b << endl;
-		cout << p << endl;
+		cout << "A()" << endl;
 	}
-private:
-	// 非静态成员变量，可以在成员声明时给缺省值。
-	int a = 10;
-	B b = 20;
-	int* p = (int*)malloc(4);
-	static int n;
+	~A()
+	{
+		cout << "~A()" << endl;
+	}
 };
-int A::n = 10;
+class B
+{
+public:
+	B()
+	{
+		cout << "B()" << endl;
+	}
+	~B()
+	{
+		cout << "~B()" << endl;
+	}
+};
+class C
+{
+public:
+	C()
+	{
+		cout << "C()" << endl;
+	}
+	~C()
+	{
+		cout << "~C()" << endl;
+	}
+};
+class D
+{
+public:
+	D()
+	{
+		cout << "D()" << endl;
+	}
+	~D()
+	{
+		cout << "~D()" << endl;
+	}
+};
+C c;
 int main()
 {
 	A a;
-	a.Print();
-	system("pause");
+	B b;
+	static D d;
+}
+//  C  D  A  B
+// `B `A `D `C  //静态的局部对象，只有在走到时才会初始化
+
+
+
+
+
+
+
+//全局变量 —— 数据段       main 函数之前就初始化   在哪都能使用，作用域是全局的
+
+//非静态的局部变量 —— 栈
+
+//静态局部变量 —— 数据段     //运行到main函数在初始化 它的作用域在test函数中，
+                           //只能在test函数中使用
+
+//那么这两个全局的有什么区别？
+//链接属性不一样：globalVar 所有文件可见
+                //staticGlobalVar 只在当前文件可见 
+//静态全局变量 —— 数据段
+
+//数组  变量 —— 栈
+
+//*char 2取的首元素地址，char是个数组，*char2 —— 栈
+
+//* + 名称 看的是后面的所在的位置
+
+int mian()
+{
+	//c语言
+	int* p1 = (int*)malloc(sizeof(int));
+	int* p3 = (int*)malloc(sizeof(int)*10);
+
+	free(p1);
+	free(p3);
+
+
+	//C++操作符
+	//int* p2 = new int ;
+	int* p2 = new int(10);      //申请一个int 4个字节空间，初始化为10
+	int* p4 = new int[10];      //申请10个int 的40个字节的空间
+
+	delete p2;
+	delete[] p4;
+
+
 	return 0;
 }
-//int main() 
-//{
-//	/*A aa(1);
-//	aa.Print();*/
-//	TestA();
-//	system("pause");
-//	return 0;
-//}
+
+//既然有了malloc和free 那new 和 delete的区别是
+//对于上面内置类型是一样的
+//对于自定义类型就有区别
+
+
+
+class A
+{
+public:
+	A()
+	{
+		_a = 0;
+		cout << "A()" << endl;
+	}
+	~A()
+	{
+		cout << "~A()" << endl;
+	}
+private:
+	int _a;
+};
+
+
+int main()
+{
+	int* p1 = new int;
+	int* p2 = (int*)malloc(sizeof(int));
+
+	A* p3 = (A*)malloc(sizeof(A));        //申请空间
+	A* p4 = new A;                        //申请空间 + 构造函数初始化
+
+	free(p3);                             //释放空间
+	delete p4;                            //析构函数
+
+	return 0;
+}
