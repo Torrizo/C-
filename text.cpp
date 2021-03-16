eae9594bb73546df99a8b5d7d5ae2b9d4764c1f7
Torrizo <55399393+Torrizo@users.noreply.github.com>
2021-03-16 23:28:13 +0800

    Add files via upload

--- text.cpp
+++ text.cpp
@@ -1,151 +1,107 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
-#include<utility>
-#include<vector>
+#include<string>
+//#include <time.h>
 using namespace std;
-
-//哈希冲突 不同的数据图通过哈希函数计算的位置相同
-//1.哈希函数不合理
-//2.空间太小
-
-//闭散列
-//先计算一个位置，如果为空直接放入，如果不为空，发生冲突，线性探测，寻找下一个空位
-enum State
-{
-	EMRTY,
-	EXIST,
-	DELETE
-};
-
-template <class K, class V>
-struct Node
-{
-	pair<K, V> _kv = pair<K,V>();
-	State _state = EMPTY;
-	
-};
-
-template <class K, class V>
-class HashTable
+////int& Add(int a, int b) {
+////	int c = a + b;
+////	return c;
+////}
+///*int main()
+//{
+//	int& ret = Add(1, 2);
+//	Add(3, 4);
+//	cout << "Add(1, 2) is :" << ret << endl;
+//	system("pause");
+//	return 0;
+//}*/
+//struct A{ int a[10000]; };
+//void TestFunc1(A a){}
+//void TestFunc2(A& a){}
+//void TestRefAndValue()
+//{
+//	A a;
+//	// 以值作为函数参数
+//	size_t begin1 = clock();
+//	for (size_t i = 0; i < 10000; ++i)
+//		TestFunc1(a);
+//	size_t end1 = clock();
+//	// 以引用作为函数参数
+//	size_t begin2 = clock();
+//	for (size_t i = 0; i < 10000; ++i)
+//		TestFunc2(a);
+//	size_t end2 = clock();
+//	// 分别计算两个函数运行结束后的时间
+//	cout << "TestFunc1(A)-time:" << end1 - begin1 << endl;
+//	cout << "TestFunc2(A&)-time:" << end2 - begin2 << endl;
+//}
+////struct A{ int a[10000]; };
+////A a;
+////// 值返回
+////A TestFunc1() { return a; }
+////// 引用返回
+////A& TestFunc2(){ return a; }
+////void TestReturnByRefOrValue()
+////{
+////	// 以值作为函数的返回值类型
+////	size_t begin1 = clock();
+////	for (size_t i = 0; i < 100000; ++i)
+////		TestFunc1();
+////	size_t end1 = clock();
+////	// 以引用作为函数的返回值类型
+////	size_t begin2 = clock();
+////	for (size_t i = 0; i < 100000; ++i)
+////		TestFunc2();
+////	size_t end2 = clock();
+////	// 计算两个函数运算完成之后的时间
+////	cout << "TestFunc1 time:" << end1 - begin1 << endl;
+////	cout << "TestFunc2 time:" << end2 - begin2 << endl;
+////}
+////int main()
+////{
+////	TestRefAndValue();
+////	TestReturnByRefOrValue();
+////	system("pause");
+////	return 0;
+////}
+//int TestAuto()
+//{
+//	return 10;
+//}
+//int main()
+//{
+//	int a = 10;
+//	auto b = a;
+//	auto c = 'a';
+//	auto d = TestAuto();
+//
+//	cout << typeid(b).name() << endl;
+//	cout << typeid(c).name() << endl;
+//	cout << typeid(d).name() << endl;
+//
+//	//auto e; 无法通过编译，使用auto定义变量时必须对其进行初始化
+//	system("pause");
+//	return 0;
+//}
+class String
 {
 public:
-	typedef HashNode<K, V> Node;
-	HashTable(size_t n = 5)
-		:size(0)
+	String(const char* str = "jack")
 	{
-		_ht.resize(n);
+		_str = (char*)malloc(strlen(str) + 1);
+		strcpy(_str, str);
 	}
-	//插入
-	bool insert(const pair<K, V>& kv)
+	~String()
 	{
-		//检查容量
-		checkCapcity();
-		//通过函数计算位置
-		int idx = kv.first % _ht.size();
-		//判断当前位置是否有效数据
-		//如果不为空，向后搜索
-		while (_ht[idx]._state == EXIST)
-		{
-			//判断键值是否存在
-			//如果键值当前状态为EXITS：表示数据有效，查看当前键值对是否存在
-			if (_ht[idx].state == EXIST && _ht[idx]._kv.first == kv.first)
-				return false;
-			++idx;
-			//判断是否越界
-			if (idx == _ht.size())
-				idx = 0;
-		}
-
-		//如果不是有效数据，空的位置直接删除或者直接插入
-		_ht[idx] = kv;
-		_ht[idx]._state = EXIST;
-		++_size;
-		return true;
+		cout << "~String()" << endl;
+		free(_str);
 	}
-
-	void checkCapacity()
-	{
-		//负载因子：元素个数/空间的大小一般是0.7或者更小
-		//线性探测中，负载因子一定小于七越接近1哈希冲突几率越大
-		if (_ht.size() == 0 || _size * 10 / _ht.size() >= 7)
-		{
-			//扩容
-			int newC = _ht.size() == 0 ? 5 : 2 * _ht.size();
-			//_ht.resize(newC); //只进行了扩容，数据位置发生了变化（因为哈希函数要用空间大小为参数）
-			HashTable<K, V> newHT(newC);
-			//遍历旧表中的元素，把有效元素存入新表
-			for (int i = 0 < _ht.size(); ++i)
-			{
-				if (_ht[i]._state == EXIST)
-					newHT.insert(_ht[i]._kv);
-			}
-
-			//交换信标内容
-			swap(_ht, newHT._ht);
-
-		}
-
-	}
-	//搜索
-	Node* find(const K& key)
-	{
-		//计算位置
-		int idx = key % _ht.size();
-		while (_ht[idx]._state != EMPTY)
-		{
-			//判断数据是否有效
-			if (_ht[idx]._state == EXIST && _ht[idx]._kv.first == key)
-				return &_ht[idx];
-			++idx;
-			if (idx == _ht.size)
-				idx = 0;
-		}
-		return nullptr;
-	}
-
-	//删除
-	bool erase(const K& key)
-	{
-		node->_state = DELETE;
-		if (node)
-		{
-			node->_state = DELETE;
-			--_size;
-			cout << key << "delete success!" << endl;
-			return true;
-		}
-		return false;
-	}
-
-	//插入
 private:
-	vector<Node>* _ht;
-	size_t _size;  //实际元素的个数
+	char* _str;
 };
-//开散列
-
-void tast()
-{
-	HashTable<int, int> ht;
-	ht.insert(make_pair(5, 5));
-	ht.insert(make_pair(15, 15));
-	ht.insert(make_pair(10, 10));
-	ht.insert(make_pair(7, 7));
-	ht.insert(make_pair(9, 9));
-	ht.insert(make_pair(6, 6));
-	ht.insert(make_pair(8, 8));
-
-	ht.erase(5);
-	ht.erase(15);
-	ht.erase(10);
-	ht.erase(7);
-	ht.erase(9);
-	ht.erase(6);
-	ht.erase(8);
-}
-
 int main()
 {
-	tast();
-	system("pause");
+	String s1("hello");
+	String s2 = s1;
 	return 0;
 }
