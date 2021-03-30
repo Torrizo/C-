06d6f730ab9839c96166a37576dc146ddadeba83
Torrizo <55399393+Torrizo@users.noreply.github.com>
2021-03-31 00:17:53 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,47 +1,60 @@
-////#include<bit/stdc++.h>
-//#include<iostream>
-//#include<unordered_map>
-//#include<vector>
-//
-//using namespace std;
-//int main()
-//{
-//	int n ,m;
-//	cin >> n >> m;
-//	vector<pair<int, int>> v;
-//	for (int i = 1; i <= n; i++)
-//	{
-//		int num;
-//		cin >> num;
-//		v.push_back(make_pair(num, i));
-//	}
-//	unordered_map<int, pair<int, int>> mp;
-//	for (auto item : v)
-//	{
-//		if (mp.find(item.first) == mp.end())
-//		{
-//			mp[item.first] = make_pair(item.second, item.second);
-//		}
-//		else
-//		{
-//			if (mp[item.first].first > item.second)
-//			{
-//				mp[item.first].first = item.second;
-//			}
-//			else if (mp[item.first].second<item.second)
-//			{
-//				mp[item.first].second = item.second;
-//			}
-//		}
-//	}
-//	for (int i = 0; i < m; i++)
-//	{
-//		int x;
-//		cin >> x;
-//		if (mp.find(x) != mp.end())
-//		{
-//			cout << mp[x].first << " " << mp[x].second << endl;
-//		}
-//	}
-//	return 0;
-//}
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+
+int main() {
+
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; ++i) {
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int> > parter;
+		vector<vector<int> > com;
+		class Compare {
+		public:
+			bool operator()(vector<int> &a, vector<int> &b) {
+				return a[0] > b[0];
+			}
+		};
+		sort(parter.begin(), parter.end(), Compare());
+		sort(com.begin(), com.end(), Compare());
+		for (int j = 0; j < n; ++j) {
+			int x;
+			cin >> x;
+			parter.push_back({ x, j + 1 });
+		}
+		for (int j = 0; j < m; ++j) {
+			int x;
+			cin >> x;
+			com.push_back({ x, j + 1 });
+		}
+		sort(parter.begin(), parter.end(), [](vector<int>& a, vector<int>& b) {
+			return a[0] > b[0];
+		});
+		sort(com.begin(), com.end(), [](vector<int>& a, vector<int>& b) {
+			return a[0] > b[0];
+		});
+		for (int i = 0, j = 0; i < parter.size(); ++i) {
+
+			if (com[j][0] >= parter[i][0]) {
+				parter[i][0] = com[j++][1];
+			}
+			else{
+				parter[i][0] = -1;
+			}
+		}
+		sort(parter.begin(), parter.end(), [](vector<int>& a, vector<int>& b) {
+			return a[1] < b[1];
+		});
+		for (vector<vector<int>>::iterator it = parter.begin(); it != parter.end(); ++it) {
+			cout << (*it)[0] << " ";
+		}
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
