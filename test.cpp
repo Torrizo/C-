7efb55e2794a533b02a2f517a8f62da9847d9934
Torrizo <55399393+Torrizo@users.noreply.github.com>
2021-04-05 22:27:40 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,149 +1,93 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <functional>
-#include <set>
-#include <list>
+#include<iostream>
+#include"test.h"
 using namespace std;
 
-
-//int main() {
-//
-//	int t;
-//	cin >> t;
-//	for (int i = 0; i < t; ++i) {
-//		int n, m;
-//		cin >> n >> m;
-//		vector<vector<int> > parter;
-//		vector<vector<int> > com;
-//		class Compare {
-//		public:
-//			bool operator()(vector<int> &a, vector<int> &b) {
-//				return a[0] > b[0];
-//			}
-//		};
-//		sort(parter.begin(), parter.end(), Compare());
-//		sort(com.begin(), com.end(), Compare());
-//		for (int j = 0; j < n; ++j) {
-//			int x;
-//			cin >> x;
-//			parter.push_back({ x, j + 1 });
-//		}
-//		for (int j = 0; j < m; ++j) {
-//			int x;
-//			cin >> x;
-//			com.push_back({ x, j + 1 });
-//		}
-//		sort(parter.begin(), parter.end(), [](vector<int>& a, vector<int>& b) {
-//			return a[0] > b[0];
-//		});
-//		sort(com.begin(), com.end(), [](vector<int>& a, vector<int>& b) {
-//			return a[0] > b[0];
-//		});
-//		for (int i = 0, j = 0; i < parter.size(); ++i) {
-//
-//			if (com[j][0] >= parter[i][0]) {
-//				parter[i][0] = com[j++][1];
-//			}
-//			else{
-//				parter[i][0] = -1;
-//			}
-//		}
-//		sort(parter.begin(), parter.end(), [](vector<int>& a, vector<int>& b) {
-//			return a[1] < b[1];
-//		});
-//		for (vector<vector<int>>::iterator it = parter.begin(); it != parter.end(); ++it) {
-//			cout << (*it)[0] << " ";
-//		}
-//		cout << endl;
-//	}
-//	system("pause");
-//	return 0;
-//}
-//int getAllSubstrings(string str)
-//{
-//	vector<string> v;
-//	if (str.length() == 0)
-//	{
-//		return;
-//	}
-//	else
-//	{
-//		for (int i = 0; i < str.length(); i++)
-//		{
-//			for (int j = 1; j < str.length() - i + 1; j++)
-//			{
-//				//std::cout << str.substr(i, j) << std::endl;
-//				v.push_back(str.substr(i, j));
-//			}
-//		}
-//	}
-//	return v;
-//}
-
+//模板不支持分离编译：生命和定义都写在头文件里面
+void test()
+{
+	int a = add(1, 2);
+}
+//含有模板的代码，声明和定义不能分开，声明和定义要写在.h/,hpp里面
 int main()
 {
-	int r = 0;
-	//char *p;
-	int n;
-	int k;
-	//int num = 0;
-	//int i;
-	cin >> n >> k;
-	vector<int> vec;
-	//vector<int> w;
-	for (int i = 0; i < n; i++)
-	{
-		int a;
-		cin >> a;
-		vec.push_back(a);
-	} //插入
-	for (int i = 0; i < n; i++)
-	{
-		string s;
-		for (int j = 1; j <= vec[i]; j++)
-		{
-			if (vec[i] % j == 0)
-			{
-				//w.push_back(v);
-				s.push_back(j);
-			}
-		}//求正整数因子
-		char p = s.c_str;
-		int num = 0;
-		while (*p != 0)
-		{
-			num = num * 10 + *p - '0';
-			p++;
-		} //将所有正整数因子合并成一个数
-		string std;
-		std.push_back(num);
-		set <int> st;
-		if (std.length() == 0)
-		{
-			r = r;
-		}
-		else
-		{
-			for (int i = 0; i < std.length(); i++)
-			{
-				for (int j = 1; j < std.length() - i + 1; j++)
-				{
-					//std::cout << str.substr(i, j) << std::endl;
-					for (int i = 0; i < std.size(); i++);
-					{
-						int s = 0;
-						int a = std[s];
-						st.insert(a);
-					}
-				}
-			}
-		}
-		set<int>::iterator it = st.find(k);
-		r++;
-	}
-	cout << r << endl;
-	system("pause");
+	test();
 	return 0;
 }
+
+//template <class T1，class T2>
+//class Data
+//{
+//public:
+//	Data(){ cout << "Data<T1,T2>" << endl; }
+//private:
+//	T1 _d1;
+//	T2 _d2;
+//};
+//
+//
+////半特化
+////对模板参数做进一步的限制
+//template <class T1，class T2>
+//class Data<T1*,T2*>
+//{
+//public:
+//	Data(){ cout << "Data<T1,T2>" << endl; }
+//private:
+//	T1 _d1;
+//	T2 _d2;
+//};
+//
+//template <class T1，class T2>
+//class Data<T1&, T2&>
+//{
+//public:
+//	Data(){ cout << "Data<T1,T2>" << endl; }
+//private:
+//	T1 _d1;
+//	T2 _d2;
+//};
+//
+//void test()
+//{
+//	Date<int, char >d1;
+//	Date<int&, char&>d2;
+//	Date<int*, char*>d3;
+//}
+////半特化
+////特化部分类
+//template<class T>
+//class Data<T, char>
+//{
+//public:
+//	Data(){ cout << "Data<T,char>" << endl; }
+//private:
+//	T _d1;
+//	char _d2;
+//};
+//
+//void test()
+//{
+//	Data<int, int>d1;
+//	Data<char, char>d2;
+//	Data<int, char>d3;
+//	//这样编辑器会报错，只能屏蔽到一个模板
+//	Data<double, double>d4; //普通模板实例化
+//}
+////模板类的全特化
+////必须有一个普通的模板类
+//template<>
+//class Data<int, char>
+//{
+//public:
+//	Data(){ cout << "Data<int,char>" << endl; }
+//private:
+//	int _d1;
+//	char _d2;
+//};
+//
+//
+//void TestVector()
+//{
+//	Date<int, int> d1; //普通类模版
+//	Date<int, char>d2; //特化（全特化）
+//}
