cf24ed72c5915293deec557fae742f31845b1063
Torrizo <55399393+Torrizo@users.noreply.github.com>
2021-04-09 00:03:30 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,93 +1,20 @@
-#include<iostream>
-#include"test.h"
-using namespace std;
-
-//模板不支持分离编译：生命和定义都写在头文件里面
-void test()
-{
-	int a = add(1, 2);
-}
-//含有模板的代码，声明和定义不能分开，声明和定义要写在.h/,hpp里面
-int main()
-{
-	test();
-	return 0;
-}
-
-//template <class T1，class T2>
-//class Data
+//#include <iostream>
+//using namespace std;
+//int nums[255];
+//int fun(int n)
 //{
-//public:
-//	Data(){ cout << "Data<T1,T2>" << endl; }
-//private:
-//	T1 _d1;
-//	T2 _d2;
-//};
-//
-//
-////半特化
-////对模板参数做进一步的限制
-//template <class T1，class T2>
-//class Data<T1*,T2*>
-//{
-//public:
-//	Data(){ cout << "Data<T1,T2>" << endl; }
-//private:
-//	T1 _d1;
-//	T2 _d2;
-//};
-//
-//template <class T1，class T2>
-//class Data<T1&, T2&>
-//{
-//public:
-//	Data(){ cout << "Data<T1,T2>" << endl; }
-//private:
-//	T1 _d1;
-//	T2 _d2;
-//};
-//
-//void test()
-//{
-//	Date<int, char >d1;
-//	Date<int&, char&>d2;
-//	Date<int*, char*>d3;
+//	nums[2] = 1;
+//	for (int i = 3; i <= n; i++)
+//	{
+//		nums[i] = i*nums[i - 1];
+//	}
+//	return nums[n];
 //}
-////半特化
-////特化部分类
-//template<class T>
-//class Data<T, char>
+//int main()
 //{
-//public:
-//	Data(){ cout << "Data<T,char>" << endl; }
-//private:
-//	T _d1;
-//	char _d2;
-//};
-//
-//void test()
-//{
-//	Data<int, int>d1;
-//	Data<char, char>d2;
-//	Data<int, char>d3;
-//	//这样编辑器会报错，只能屏蔽到一个模板
-//	Data<double, double>d4; //普通模板实例化
-//}
-////模板类的全特化
-////必须有一个普通的模板类
-//template<>
-//class Data<int, char>
-//{
-//public:
-//	Data(){ cout << "Data<int,char>" << endl; }
-//private:
-//	int _d1;
-//	char _d2;
-//};
-//
-//
-//void TestVector()
-//{
-//	Date<int, int> d1; //普通类模版
-//	Date<int, char>d2; //特化（全特化）
+//	int N;
+//	cin >> N;
+//	cout << fun(N) << endl;
+//	system("pause");
+//	return 0;
 //}
--- text.cpp
+++ text.cpp
@@ -1,17 +1,48 @@
-#include<iostream>
-#include<string>
-using namespace std;
-	
-	
-
-int main()
-{
-	string str1 = "hello";
-	string str2;
-	cin >> str1;
-	cin >> str2;
-	char *pos = const_cast<char*>(str1.c_str());
-	
-	
-	return 0;
-}
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//
+//
+//
+//int knapsack(int n, int time, vector<int> &v, vector<int> &vc, vector<vector<int>> &res){
+//	for (int i = 1; i <= n; i++){//i个物品 
+//		for (int j = 1; j <= time; j++){//背包容量为j 
+//			if (v[i] > j){//当前背包的容量 j 放不下第 i 件商品时
+//				res[i][j] = res[i - 1][j];
+//			}
+//			else
+//			{
+//				res[i][j] = max(res[i - 1][j - v[i]] + vc[i], res[i - 1][j]);
+//			}
+//		}
+//	}
+//
+//	return res[n][time];
+//}
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	vector<int> v;
+//	for (int i = 0; i < n; i++)
+//	{
+//		int x = 0;
+//		cin >> x;
+//		v.push_back(x);
+//	}
+//	vector<int> vc;
+//	for (int i = 0; i < n; i++)
+//	{
+//		int x = 0;
+//		cin >> x;
+//		vc.push_back(x);
+//	}
+//	int time;
+//	cin >> time;
+//	vector<vector<int>> res(n+1, vector<int>(time + 1), 0);
+//	knapsack(n, time, v, vc, res);
+//	cout << res[n][time] << endl;
+//	system("pause");
+//	return 0;
+//}
