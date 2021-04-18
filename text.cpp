bebf0dbe311c181236b4b9267a7a09cc640b58ee
Torrizo <55399393+Torrizo@users.noreply.github.com>
2021-04-18 21:46:02 +0800

    Add files via upload

--- text.cpp
+++ text.cpp
@@ -1,61 +1,105 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
-#include<set>
-#include<queue>
+#include<list>
+#include<string>
 using namespace std;
-void kinghappy()
+
+void pingpang()
 {
-	int n, T;
-	cin >> n >> T;
-	queue<int> time;
-	set<int> v;
-	for (int i = 0; i < n; i++)
+	int x;
+	cin >> x;
+	for (int i = 0; i < x; i++)
 	{
-		int x = 0;
-		cin >> x;
-		v.insert(x);
-	}
-	for (int i = 0; i < n-1; i++)
-	{
-		int U, V;
-		int x = 0;
-		cin >> U >> V >> x;
-		time.push(x);
-	}
-	int c = 0;
-	int i = -1;
-	while (T >= 2*c)
-	{
-		c += time.front();
-		time.pop();
-		i++;
-	}
-	if (i>0)
-	{
-		set <int>::iterator it;
-		vector<int>s;
-		for (it = v.begin(); it != v.find(i); it++)
+		vector<int> a;
+		vector<int> b;
+		for (int j = 0; j < 6; j++)
 		{
-			s.push_back(*it);
-			//cout << *it << endl;
+			int z = 0;
+			int c = 0;
+			cin >> z;
+			cin >> c;
+			a.push_back(z);
+			b.push_back(c);
 		}
-		cout << s.back() << endl;
-	}
-	else
-	{
-		set <int>::iterator it;
-		vector<int>s;
-		for (it = v.begin(); it != v.find(2); it++)
-		{
-			s.push_back(*it);
-			//cout << *it << endl;
-		}
-		cout << s.back() << endl;
+		sort(a.begin(), a.end());
+		sort(b.begin(), b.end());
+		int n = 0;
+		int m = 0;
+		int u = 0;
+		int t = 0;
+
+		//for (int j = 0; j < 3; j++)
+		//{
+		//	int	q = 0;
+		//	int d = 0;
+		//    if(b[1] - a[j] - (b[0] - a[j])> 0)
+		//	{
+		//		q++;
+		//	}
+		//	else if (b[2] - a[j] - (b[1] - a[j])>0 || b[2] - a[j] - b[0] - a[j]> 0)
+		//	{
+		//		d++;
+		//	}
+		//	n = q*d;
+		//}
+		//for (int j = 0; j < 3; j++)
+		//{
+		//	int	q = 0;
+		//	int d = 0;
+		//	if (b[4] - a[j+3] - (b[3] - a[j+3])> 0)
+		//	{
+		//		q++;
+		//	}
+		//	else if (b[5] - a[j+3] - (b[4] - a[j+3])>0 || b[5] - a[j+3] - b[3] - a[j+3]> 0)
+		//	{
+		//		d++;
+		//	}
+		//	m = q*d;
+		//}
+		//for (int j = 0; j < 6; j++)
+		//{
+		//	int p = 0;
+		//	for (int k = 0; k < 6; k++)
+		//	{
+		//		if ((b[j] + b[k]) - (a[k] + a[j]) > 0)
+		//		{
+		//			p++;
+		//		}
+		//		u = p;
+		//	}
+		//}
+		//if (n == 0)
+		//{
+		//	t = u*m;
+		//	cout << t << endl;
+		//}
+		//else if (m == 0)
+		//{
+		//	t = u*n;
+		//	cout << t << endl;
+		//}
+		//else if (u == 0)
+		//{
+		//	t = m*n;
+		//	cout << t << endl;
+		//}
+		//else if (n == 0 && m == 0 || n == 0 && u == 0 || m == 0 && u == 0)
+		//{
+		//	t = 0;
+		//	cout << t << endl;
+		//}
+		//else 
+		//{
+		//	t = 0;
+		//	cout << t << endl;
+		//}
 	}
 }
+
 int main()
 {
-	kinghappy();
+	pingpang();
 	system("pause");
 	return 0;
 }
