ca3d7cc8b70636f04449d11d3ccb010fda92afbf
Torrizo <55399393+Torrizo@users.noreply.github.com>
2021-04-19 22:49:34 +0800

    Add files via upload

--- text.cpp
+++ text.cpp
@@ -1,105 +1,135 @@
 #include<iostream>
-#include<algorithm>
-#include<vector>
-#include<list>
-#include<string>
 using namespace std;
 
-void pingpang()
+//class Person
+//{
+//public:
+//	Person() { ++_count; }
+//protected:
+//	string _name; // 姓名
+//public:
+//	static int _count; // 统计人的个数。
+//};
+//int Person::_count = 0;
+//class Student : public Person
+//{
+//protected:
+//	int _stuNum; // 学号
+//};
+//class Graduate : public Student
+//{
+//protected:
+//	string _seminarCourse; // 研究科目
+//};
+////class Car
+////{
+////public:
+////	virtual void Drive() final {}
+////};
+////class Benz :public Car
+////{
+////public:
+////	virtual void Drive() { cout << "Benz-舒适" << endl; }
+////};
+////class Car{
+////public:
+////	virtual void Drive(){}
+////};
+////class Benz :public Car {
+////public:
+////	virtual void Drive() override { cout << "Benz-舒适" << endl; }
+////};
+//class Base
+//{
+//public:
+//	virtual void Func1()
+//	{
+//		cout << "Func1()" << endl;
+//	}
+//private:
+//	int _b = 1;
+//};
+//void TestPerson()
+//{
+//	Student s1;
+//	Student s2;
+//	Student s3;
+//	Graduate s4;
+//	cout << " 人数 :" << Person::_count << endl;
+//	Student::_count = 0;
+//	cout << " 人数 :" << Person::_count << endl;
+//}
+//class Car
+//{
+//public:
+//	virtual void Drive() = 0;
+//};
+//class Benz :public Car
+//{
+//public:
+//	 void Drive()
+//	{
+//		cout << "Benz-舒适" << endl;
+//	}
+//};
+//class BMW :public Car
+//{
+//public:
+//	 void Drive()
+//	{
+//		cout << "BMW-操控" << endl;
+//	}
+//};
+//void Test()
+//{
+//	Car* pBenz = new Benz;
+//	pBenz->Drive();
+//	Car* pBMW = new BMW;
+//	pBMW->Drive();
+//}
+class Base
 {
-	int x;
-	cin >> x;
-	for (int i = 0; i < x; i++)
+public:
+	virtual void func1() { cout << "Base::func1" << endl; }
+	virtual void func2() { cout << "Base::func2" << endl; }
+private:
+	int a;
+};
+class Derive :public Base {
+public:
+	virtual void func1() { cout << "Derive::func1" << endl; }
+	virtual void func3() { cout << "Derive::func3" << endl; }
+	virtual void func4() { cout << "Derive::func4" << endl; }
+private:
+	int b;
+};
+typedef void(*VFPTR) ();
+void PrintVTable(VFPTR vTable[])
+{
+	// 依次取虚表中的虚函数指针打印并调用。调用就可以看出存的是哪个函数
+	cout << " 虚表地址>" << vTable << endl;
+	for (int i = 0; vTable[i] != nullptr; ++i)
 	{
-		vector<int> a;
-		vector<int> b;
-		for (int j = 0; j < 6; j++)
-		{
-			int z = 0;
-			int c = 0;
-			cin >> z;
-			cin >> c;
-			a.push_back(z);
-			b.push_back(c);
-		}
-		sort(a.begin(), a.end());
-		sort(b.begin(), b.end());
-		int n = 0;
-		int m = 0;
-		int u = 0;
-		int t = 0;
-
-		//for (int j = 0; j < 3; j++)
-		//{
-		//	int	q = 0;
-		//	int d = 0;
-		//    if(b[1] - a[j] - (b[0] - a[j])> 0)
-		//	{
-		//		q++;
-		//	}
-		//	else if (b[2] - a[j] - (b[1] - a[j])>0 || b[2] - a[j] - b[0] - a[j]> 0)
-		//	{
-		//		d++;
-		//	}
-		//	n = q*d;
-		//}
-		//for (int j = 0; j < 3; j++)
-		//{
-		//	int	q = 0;
-		//	int d = 0;
-		//	if (b[4] - a[j+3] - (b[3] - a[j+3])> 0)
-		//	{
-		//		q++;
-		//	}
-		//	else if (b[5] - a[j+3] - (b[4] - a[j+3])>0 || b[5] - a[j+3] - b[3] - a[j+3]> 0)
-		//	{
-		//		d++;
-		//	}
-		//	m = q*d;
-		//}
-		//for (int j = 0; j < 6; j++)
-		//{
-		//	int p = 0;
-		//	for (int k = 0; k < 6; k++)
-		//	{
-		//		if ((b[j] + b[k]) - (a[k] + a[j]) > 0)
-		//		{
-		//			p++;
-		//		}
-		//		u = p;
-		//	}
-		//}
-		//if (n == 0)
-		//{
-		//	t = u*m;
-		//	cout << t << endl;
-		//}
-		//else if (m == 0)
-		//{
-		//	t = u*n;
-		//	cout << t << endl;
-		//}
-		//else if (u == 0)
-		//{
-		//	t = m*n;
-		//	cout << t << endl;
-		//}
-		//else if (n == 0 && m == 0 || n == 0 && u == 0 || m == 0 && u == 0)
-		//{
-		//	t = 0;
-		//	cout << t << endl;
-		//}
-		//else 
-		//{
-		//	t = 0;
-		//	cout << t << endl;
-		//}
+		printf(" 第%d个虚函数地址 :0X%x,->", i, vTable[i]);
+		VFPTR f = vTable[i];
+		f();
 	}
+	cout << endl;
 }
-
 int main()
 {
-	pingpang();
-	system("pause");
+	Base b;
+	Derive d;
+	VFPTR* vTableb = (VFPTR*)(*(int*)&b);
+	PrintVTable(vTableb);
+	VFPTR* vTabled = (VFPTR*)(*(int*)&d);
+	PrintVTable(vTabled);
 	return 0;
 }
+//int main()
+//{
+//	//cout << sizeof(Base) << endl;
+//	Test();
+//	system("pause");
+//	return 0;
+//}
