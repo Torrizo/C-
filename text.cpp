d8610a3fe3815df1850f6dc4e73b0b3b17227f3a
Torrizo <55399393+Torrizo@users.noreply.github.com>
2021-04-24 21:00:37 +0800

    Add files via upload

--- text.cpp
+++ text.cpp
@@ -1,71 +1,15 @@
-////#include<iostream>
-////
-////using namespace std;
-////class Base {
-////public:
-////	void f() {
-////		cout << "Base\n";
-////	}
-////};
-////
-////class Derived :public Base {
-////public:
-////	void f() {
-////		cout << "Derived\n";
-////	}
-////};
-////
-////
-////using namespace std;
-////int main() {
-////	class student {
-////		int rno = 10;
-////	} v;
-////
-////	cout << v.rno;
-////}
-//////int main() {
-//////	Base *p = new Derived();  
-//////   
-//////	p->f();
-//////	system("pause");
-//////	return 0;
-//////}
-//#include<iostream>
-//
-//using namespace std;
-//class Base {
-//public:
-//	void f() {
-//		cout << "Base\n";
-//	}
-//};
-//class Derived :public Base {
-//public:
-//	void f() {
-//		cout << "Derived\n";
-//	}
-//};
-//int main() {
-//	Derived obj;
-//	obj.Base::f();
-//	system("pause");
-//	return 0;
-//}
 #include<iostream>
-
+#define TEST1(X,Y) X+Y
+#define TEST2(X) #X
+#define TEST3(X,Y) X##Y
+#define TEST4(X,Y) (X+Y)
 using namespace std;
-int main() {
-	int i;
-    char s[] = "hello";
 
-    for (i = 0; s[i]; ++i);
-    cout << i << endl;
-
-    i = 0;
-	cout << sizeof(s);
-    while (s[i++]);
-    cout << i;
+int main()
+{
+	string s = "XXX";
+	int i = 1 - TEST4(1, 2)*TEST1(5, 6) * 2;
+	cout << i << endl;
 	system("pause");
 	return 0;
 }
