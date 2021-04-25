36753fff679686c60b81d30482e43ecf955fb34a
Torrizo <55399393+Torrizo@users.noreply.github.com>
2021-04-25 22:24:12 +0800

    Add files via upload

--- text.cpp
+++ text.cpp
@@ -1,15 +1,82 @@
 #include<iostream>
-#define TEST1(X,Y) X+Y
-#define TEST2(X) #X
-#define TEST3(X,Y) X##Y
-#define TEST4(X,Y) (X+Y)
+#include<vector>
 using namespace std;
+//int j = 0;
+//int fun(int x ,vector<int>&v)
+//{
+//	int max = v[0];
+//	int min = v[0];
+//	int m = 0;
+//	int n = 0;
+//	for (int i = 0; i < x; i++)
+//	{
+//		if (v[i]>max)
+//		{
+//			max = v[i];
+//			n=i;
+//		}
+//	}
+//	for (int i = 0; i < x; i++)
+//	{
+//		if (v[i]<min)
+//		{
+//			min = v[i];
+//			m=i;
+//		}
+//	}
+//	//cout << max << endl;
+//	if ((max + min) % 2 == 0)
+//	{
+//		v[m] = (max + min) / 2;
+//		v[n] = (max + min) / 2;
+//	}
+//	else
+//	{
+//		v[m] = (max + min) / 2;
+//		v[n] = (max + min) / 2 + 1;
+//	}
+//	for (int i = 0; i < x-1; i++)
+//	{
+//		if (v[i] == v[i + 1])
+//		{
+//			return fun(x,v);
+//			j++;
+//			break;
+//		}
+//		else
+//		{
+//			cout << j << endl;
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int x;
+//	cin >> x;
+//	vector<int>v;
+//	for (int i = 0; i < x; i++)
+//	{
+//		int u;
+//		cin >> u;
+//		v.push_back(u);
+//	}
+//	fun(x,v);
+//	system("pause");
+//	return 0;
+//}
 
+class A{
+public:
+	virtual void fun_1(){}
+	virtual void fun_2(){}
+
+};
+class B :public A{
+
+};
 int main()
 {
-	string s = "XXX";
-	int i = 1 - TEST4(1, 2)*TEST1(5, 6) * 2;
-	cout << i << endl;
-	system("pause");
+	cout << sizeof(A) << endl;
 	return 0;
 }
