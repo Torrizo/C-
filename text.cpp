317033c94c78b64913e111b0f678ae7d3c2998d4
Torrizo <55399393+Torrizo@users.noreply.github.com>
2021-05-15 23:28:49 +0800

    Add files via upload

--- text.cpp
+++ text.cpp
@@ -1,31 +1,71 @@
+////#include<iostream>
+////
+////using namespace std;
+////class Base {
+////public:
+////	void f() {
+////		cout << "Base\n";
+////	}
+////};
+////
+////class Derived :public Base {
+////public:
+////	void f() {
+////		cout << "Derived\n";
+////	}
+////};
+////
+////
+////using namespace std;
+////int main() {
+////	class student {
+////		int rno = 10;
+////	} v;
+////
+////	cout << v.rno;
+////}
+//////int main() {
+//////	Base *p = new Derived();  
+//////   
+//////	p->f();
+//////	system("pause");
+//////	return 0;
+//////}
+//#include<iostream>
+//
+//using namespace std;
+//class Base {
+//public:
+//	void f() {
+//		cout << "Base\n";
+//	}
+//};
+//class Derived :public Base {
+//public:
+//	void f() {
+//		cout << "Derived\n";
+//	}
+//};
+//int main() {
+//	Derived obj;
+//	obj.Base::f();
+//	system("pause");
+//	return 0;
+//}
 #include<iostream>
-#include<vector>
+
 using namespace std;
-int test(int v, int* a,int n)
-{
-	if (v > a[n - 1])
-	{
-		return n + 1;
-	}
-	int left = 0;
-	int right = n - 1;
-	int mid = (left + right) / 2;
-	while (left < right)
-	{
-		if (a[mid] >= v)
-		{
-			right = mid;
-		}
-		else
-		{
-			left = mid + 1;
-		}
-		mid = left + (right - left) / 2;
-	}
-	return mid + 1;
-}
-int main()
-{
-	vector<int> v;
+int main() {
+	int i;
+    char s[] = "hello";
+
+    for (i = 0; s[i]; ++i);
+    cout << i << endl;
+
+    i = 0;
+	cout << sizeof(s);
+    while (s[i++]);
+    cout << i;
+	system("pause");
 	return 0;
 }
