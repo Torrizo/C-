53daebd678fc3214bc050b15cca35ad736c8e611
Torrizo <55399393+Torrizo@users.noreply.github.com>
2021-05-17 23:26:15 +0800

    Add files via upload

--- text.cpp
+++ text.cpp
@@ -1,82 +1,48 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-//int j = 0;
-//int fun(int x ,vector<int>&v)
-//{
-//	int max = v[0];
-//	int min = v[0];
-//	int m = 0;
-//	int n = 0;
-//	for (int i = 0; i < x; i++)
-//	{
-//		if (v[i]>max)
-//		{
-//			max = v[i];
-//			n=i;
-//		}
-//	}
-//	for (int i = 0; i < x; i++)
-//	{
-//		if (v[i]<min)
-//		{
-//			min = v[i];
-//			m=i;
-//		}
-//	}
-//	//cout << max << endl;
-//	if ((max + min) % 2 == 0)
-//	{
-//		v[m] = (max + min) / 2;
-//		v[n] = (max + min) / 2;
-//	}
-//	else
-//	{
-//		v[m] = (max + min) / 2;
-//		v[n] = (max + min) / 2 + 1;
-//	}
-//	for (int i = 0; i < x-1; i++)
-//	{
-//		if (v[i] == v[i + 1])
-//		{
-//			return fun(x,v);
-//			j++;
-//			break;
-//		}
-//		else
-//		{
-//			cout << j << endl;
-//		}
-//	}
-//}
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
 //
+//
+//
+//int knapsack(int n, int time, vector<int> &v, vector<int> &vc, vector<vector<int>> &res){
+//	for (int i = 1; i <= n; i++){//i个物品 
+//		for (int j = 1; j <= time; j++){//背包容量为j 
+//			if (v[i] > j){//当前背包的容量 j 放不下第 i 件商品时
+//				res[i][j] = res[i - 1][j];
+//			}
+//			else
+//			{
+//				res[i][j] = max(res[i - 1][j - v[i]] + vc[i], res[i - 1][j]);
+//			}
+//		}
+//	}
+//
+//	return res[n][time];
+//}
 //int main()
 //{
-//	int x;
-//	cin >> x;
-//	vector<int>v;
-//	for (int i = 0; i < x; i++)
+//	int n;
+//	cin >> n;
+//	vector<int> v;
+//	for (int i = 0; i < n; i++)
 //	{
-//		int u;
-//		cin >> u;
-//		v.push_back(u);
+//		int x = 0;
+//		cin >> x;
+//		v.push_back(x);
 //	}
-//	fun(x,v);
+//	vector<int> vc;
+//	for (int i = 0; i < n; i++)
+//	{
+//		int x = 0;
+//		cin >> x;
+//		vc.push_back(x);
+//	}
+//	int time;
+//	cin >> time;
+//	vector<vector<int>> res(n+1, vector<int>(time + 1), 0);
+//	knapsack(n, time, v, vc, res);
+//	cout << res[n][time] << endl;
 //	system("pause");
 //	return 0;
 //}
-
-class A{
-public:
-	virtual void fun_1(){}
-	virtual void fun_2(){}
-
-};
-class B :public A{
-
-};
-int main()
-{
-	cout << sizeof(A) << endl;
-	return 0;
-}
