d85142500d6545c1db44eb4191b9a2ab021f835e
Torrizo <55399393+Torrizo@users.noreply.github.com>
2021-05-19 23:14:23 +0800

    Add files via upload

--- text.cpp
+++ text.cpp
@@ -1,107 +1,70 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
-#include<string>
-//#include <time.h>
 using namespace std;
-////int& Add(int a, int b) {
-////	int c = a + b;
-////	return c;
-////}
-///*int main()
-//{
-//	int& ret = Add(1, 2);
-//	Add(3, 4);
-//	cout << "Add(1, 2) is :" << ret << endl;
-//	system("pause");
-//	return 0;
-//}*/
-//struct A{ int a[10000]; };
-//void TestFunc1(A a){}
-//void TestFunc2(A& a){}
-//void TestRefAndValue()
-//{
-//	A a;
-//	// 以值作为函数参数
-//	size_t begin1 = clock();
-//	for (size_t i = 0; i < 10000; ++i)
-//		TestFunc1(a);
-//	size_t end1 = clock();
-//	// 以引用作为函数参数
-//	size_t begin2 = clock();
-//	for (size_t i = 0; i < 10000; ++i)
-//		TestFunc2(a);
-//	size_t end2 = clock();
-//	// 分别计算两个函数运行结束后的时间
-//	cout << "TestFunc1(A)-time:" << end1 - begin1 << endl;
-//	cout << "TestFunc2(A&)-time:" << end2 - begin2 << endl;
-//}
-////struct A{ int a[10000]; };
-////A a;
-////// 值返回
-////A TestFunc1() { return a; }
-////// 引用返回
-////A& TestFunc2(){ return a; }
-////void TestReturnByRefOrValue()
-////{
-////	// 以值作为函数的返回值类型
-////	size_t begin1 = clock();
-////	for (size_t i = 0; i < 100000; ++i)
-////		TestFunc1();
-////	size_t end1 = clock();
-////	// 以引用作为函数的返回值类型
-////	size_t begin2 = clock();
-////	for (size_t i = 0; i < 100000; ++i)
-////		TestFunc2();
-////	size_t end2 = clock();
-////	// 计算两个函数运算完成之后的时间
-////	cout << "TestFunc1 time:" << end1 - begin1 << endl;
-////	cout << "TestFunc2 time:" << end2 - begin2 << endl;
-////}
-////int main()
-////{
-////	TestRefAndValue();
-////	TestReturnByRefOrValue();
-////	system("pause");
-////	return 0;
-////}
-//int TestAuto()
-//{
-//	return 10;
-//}
-//int main()
-//{
-//	int a = 10;
-//	auto b = a;
-//	auto c = 'a';
-//	auto d = TestAuto();
+//class A {
+//public:
+//	A(int a)
+//		:_a1(a)
+//		, _a2(_a1)
+//	{}
 //
-//	cout << typeid(b).name() << endl;
-//	cout << typeid(c).name() << endl;
-//	cout << typeid(d).name() << endl;
-//
-//	//auto e; 无法通过编译，使用auto定义变量时必须对其进行初始化
-//	system("pause");
-//	return 0;
+//	void Print() {
+//		cout << _a1 << " " << _a2 << endl;
+//	}
+//private:
+//	int _a2;
+//	int _a1;
+//};
+//class A {
+//public:
+//	A() { ++_scount; }
+//	A(const A& t) { ++_scount; }
+//	static int GetACount() { return _scount; }
+//private:
+//	static int _scount;
+//};
+//int A::_scount = 0;
+//void TestA()
+//{
+//	cout << A::GetACount() << endl;
+//	A a1, a2;
+//	A a3(a1);
+//	cout << A::GetACount() << endl;
 //}
-class String
-{
+class B {
 public:
-	String(const char* str = "jack")
+	B(int b = 0)
+		:_b(b)
+	{}
+	int _b;
+};
+class A {
+public:
+	void Print()
 	{
-		_str = (char*)malloc(strlen(str) + 1);
-		strcpy(_str, str);
-	}
-	~String()
-	{
-		cout << "~String()" << endl;
-		free(_str);
+		cout << a << endl;
+		cout << b._b << endl;
+		cout << p << endl;
 	}
 private:
-	char* _str;
+	// 非静态成员变量，可以在成员声明时给缺省值。
+	int a = 10;
+	B b = 20;
+	int* p = (int*)malloc(4);
+	static int n;
 };
+int A::n = 10;
 int main()
 {
-	String s1("hello");
-	String s2 = s1;
+	A a;
+	a.Print();
+	system("pause");
 	return 0;
 }
+//int main() 
+//{
+//	/*A aa(1);
+//	aa.Print();*/
+//	TestA();
+//	system("pause");
+//	return 0;
+//}
