72d7517c53da40dda6e7ff33eed6c9436704ea02
Torrizo <55399393+Torrizo@users.noreply.github.com>
2021-05-26 21:52:56 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,69 +1,24 @@
 #include<iostream>
-#include<vector>
-#include<list>
 using namespace std;
-int test(int num1, int num2, int num3, int num4)
+int test()
 {
-	int a, b, c;
-	a = (num1 + num3) / 2;
-	b = (num2 + num4) / 2;
-	c = (num4 - num2) / 2;
-	if ((a - ((num1 + num3) / 2)) != 0)
-	{
-		cout << "No" << endl;
-	}
-	else if ((b - ((num2 + num4) / 2)) != 0)
-	{
-		cout << "No" << endl;
-	}
-	else if ((c - ((num4 - num2) / 2)) != 0)
-	{
-		cout << "No" << endl;
-	}
-	else
-	{
-		cout << "" << a;
-		cout << "" << b;
-		cout << "" << c;
-	}
-	
-	return 0;
+	double v1, v2, c1, c2, s;
+	cin >> v1 >> v2 >> c1 >> c2 >> s;
+	double k, b, d ,f;
+	f = 8.34;
+	k = (c2 - c1) / (v2 - v1);
+	b = k*s / f;
+	d = (k*v1 - 2 * c1)*s / f - v1*v1;
+	cout << "k=" << k << " " << "b=" << b << " " << "d=" << d << " " << endl;
+	return k, b, d;
 }
-
-//
-//int main()
-//{
-//	int num1, num2, num3, num4;
-//	cin >> num1;
-//	cin >> num2;
-//	cin >> num3;
-//	cin >> num4;
-//	test(num1,num2,num3,num4);
-//	system("pause");
-//	return 0;
-//}
 
 int main()
 {
-	string s = "", table = "0123456789ABCDEF";
-	int M = 0, N = 0;
-	cin >> M >> N;
-
-	if (M == 0)
+	while (1)
 	{
-		s = "0";
+		test();
 	}
-
-	while (M)
-	{
-		if (M < 0)
-		{
-			M = -M;
-			cout << "-";
-		}
-		s = table[M % N] + s;
-		M /= N;
-	}
-	cout << s << endl;
+	system("pause");
 	return 0;
 }
